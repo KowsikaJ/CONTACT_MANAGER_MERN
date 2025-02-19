0e22ea04f1f58cc9ba278cae4aa924c9b6bdab9e
KowsikaJ <kowsikaj05@gmail.com>
2025-02-19 19:55:14 +0530

    updated app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Home from './Pages/Home'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';  // ✅ Import Login
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]);
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+    <RouterProvider router={router} />  // ✅ Use RouterProvider to enable routing
+  );
+};
 
-export default App
+export default App;

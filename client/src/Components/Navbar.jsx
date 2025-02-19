7498604ede92461a0ecd35e0fc7fd69b247f623a
KowsikaJ <kowsikaj05@gmail.com>
2025-02-19 16:36:18 +0530

    navbar.js

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="navbar-left">
+            <a href="" className='navbar-brand'>
+                CONTACT MANAGER
+            </a>
+        </div>
+        <div className="navbar-right">
+            <a href="" className='navbar-link'>About</a>
+            <a href="" className='navbar-link'>Login</a>
+            <a href="" className='navbar-link'>Register</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
